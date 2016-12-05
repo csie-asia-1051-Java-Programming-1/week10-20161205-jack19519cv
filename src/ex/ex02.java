a47d33dacb4534bdd2a6807845d750a6f4534ce2
jack19519cv <105021059@live.asia.edu.tw>
2016-12-05 16:51:03 +0800

    save

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,14 +1,44 @@
 ﻿package ex;
 /*
  * Topic: 輸入所使用的度數，換算夏月及非夏月之電費金額。
- * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
- */
 
+ * Date: 2016/12/05
+ * Author: 105021059  王俊鑌
+ */
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n =scn.nextInt();
+		
+		if(n<120){
+		
+			System.out.println( n*2.10);
+		System.out.println( n*2.10);
+		}else if(n>=121&&n<=330){
+			System.out.println( (n-120)*3.02+120*2.10);
+			System.out.println( (n-120)*2.68+120*2.10);
+					
+		
+			
+		}else if(n>=331&&n<=500){
+			System.out.println( (n-330)*4.39+210*3.02+120*2.10);
+			System.out.println( (n-330)*3.61+210*2.68+120*2.10);
+			
+		}else if(n>=501&&n<=700){
+			System.out.println( (n-500)*4.97+170*4.39+210*3.02+120*2.10);
+			System.out.println( (n-500)*4.01+170*3.61+210*2.68+120*2.10);
+			
+		}else if(n>=701){
+			System.out.println( (n-700)*5.63+200*4.97+170*4.39+210*3.02+120*2.10);
+			System.out.println( (n-700)*4.50+200*4.01+170*3.61+210*2.68+120*2.10);
+			
+		}
+		
+				
+		
+		
 
 	}
 
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -1,15 +1,69 @@
 ﻿package ex;
 /*
  * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
- * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
- */
 
+ * Date: 2016/12/05
+ * Author: 105021059  王俊鑌
+ */
+import java.util.ArrayList;
+
+import java.util.Scanner;
 public class ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner scn = new Scanner(System.in);
+	ArrayList<Integer>dataList=new
+			ArrayList<Integer>();
+	String str = "Y";
+	int i=0,val=0;
+	boolean flag = false;
+	while(str.equals("N")||str.equals("n")){
+		System.out.print("input a value:");
+		val =scn.nextInt();
+		if(dataList.size()==0){
+			dataList.add(val);
+			
+		}else if (dataList.size()==1){
+			if(dataList.get(i)>val){
+				dataList.add(val);
+			}else{
+				dataList.add( 0 ,val );//TODO do something
+			}
+				
+			}else{flag=true; i=0;
+			while(flag&&i<dataList.size()-1){
+					if(i==0&&dataList.get(0)<val){
+						dataList.add(0,val);
+						flag=false;
+					}
+			
+			}
+		}
+		 else{                                                 //TODO THIS OR NOT
+				if(dataList.get(i)>val&&dataList.get(i+1)<=val){
+		flag=false;
+		dataList.add(i+1,val);
+				}else{
+					i++;
+				}
+				
+				
+	
+			}
+			
+		}     
+		
+		
+	}
 
 	}
 
+private static void showdata(ArrayList list){
+	int size=list.size();
+	for(int i=0;i<size;i++){
+		System.out.print(list.get(i)+"\t");
+	}
+	System.out.println();
 }
+
+
